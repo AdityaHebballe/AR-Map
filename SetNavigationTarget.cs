987e4868b88311ad537421d62ba3234ca05298be
Aditya <adityahebbale@gmail.com>
2024-10-23 18:38:27 +0530

    Added unity files

---
+++ SetNavigationTarget.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class SetNavigationTarget : MonoBehaviour
+{
+
+    Earlier there was code in this file but now for some reason I deleted it if you want this complete project or this code you can email me or contact me.
+
+
+}
